b248c7961290fac97a05c2acc595dfccc005c31a
id21lie <104353410+id21lie@users.noreply.github.com>
2025-03-09 18:58:25 +0100

    updated css for phone

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header({ project }) {
     return (
         <header className="text-center mb-12">
-        <h1 className="text-4xl font-bold">Hello, My name is <span className="text-white font-extrabold">Lisa Ihre</span></h1>
+        <h1 class="hero-title">Hello, My name is <span>Lisa Ihre</span></h1>
         <p className="mt-2 text-lg">I am studying to become a Master's engineer in (data) interaction and design.</p>
         </header>
     )
