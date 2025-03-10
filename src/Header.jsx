0549fd0203e26e13ee7786c0a70510160584287c
id21lie <104353410+id21lie@users.noreply.github.com>
2025-03-10 10:31:03 +0100

    updated css

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ export default function Header({ project }) {
     return (
         <header className="header">
             <h1 className="hero-title">Hello, My name is <span>Lisa Ihre</span></h1>
-            <p>I am studying to become a Master's engineer in (data) interaction and design.</p>
+            <p>I am studying to become a Master's engineer in interaction and design.</p>
         </header>
     )
 }
